b6c0bc8935f0a1d9659bd0848fe7f3c2a78ce5cc
JaaiDead <arcticquests.dev@gmail.com>
2025-06-18 22:55:50 +0530

    Final Draft 3.0 :D ;p

--- src/main/java/org/arcticquests/dev/block/wood/ModWoodTypes.java
+++ src/main/java/org/arcticquests/dev/block/wood/ModWoodTypes.java
@@ -1,12 +1,10 @@
 package org.arcticquests.dev.block.wood;
 
-import net.fabricmc.fabric.api.object.builder.v1.block.type.WoodTypeBuilder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.state.properties.WoodType;
 import org.arcticquests.dev.PerfectParityPG;
 
 public class ModWoodTypes {
-    private static final WoodTypeBuilder woodTypeBuilder = new WoodTypeBuilder();
-    public static final WoodType  PALE_OAK = woodTypeBuilder.register(ResourceLocation.fromNamespaceAndPath(PerfectParityPG.MODID,"pale_oak"), ModBlockSetTypes.PALE_OAK);
+    public static final WoodType PALE_OAK = WoodType.register(new WoodType(ResourceLocation.fromNamespaceAndPath(PerfectParityPG.MODID,"pale_oak").toString(),ModBlockSetTypes.PALE_OAK));
 
 }
